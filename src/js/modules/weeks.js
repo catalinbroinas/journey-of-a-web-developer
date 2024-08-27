f529c3bcebbaf62ea11028f4c28aa147538025ce
catalinbroinas <catalinbroinas@yahoo.com>
2024-08-27 11:13:17 +0300

    Add `weeks.js` module: handling data & DOM rendering - WeekDataProvider: loading weekly data - WeekDomManager: building and rendering week elements

---
+++ src/js/modules/weeks.js
@@ -0,0 +1,21 @@
+async function WeekDataProvider() {
+    const loadWeeksData = () => {
+
+    };
+
+    return { loadWeeksData };
+}
+
+function WeekDomManager() {
+    const buildWeekElement = () => {
+
+    };
+
+    const renderWeeks = () => {
+
+    };
+
+    return { renderWeeks };
+}
+
+export { WeekDataProvider, WeekDomManager };
