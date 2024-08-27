50775b88038f6508cf7e0dc772c2d40cfb4dd6a3
catalinbroinas <catalinbroinas@yahoo.com>
2024-08-27 18:33:55 +0300

    Implement `loadWeeksData` for fetching weekly data

--- src/js/modules/weeks.js
+++ src/js/modules/weeks.js
@@ -1,6 +1,16 @@
 async function WeekDataProvider() {
-    const loadWeeksData = () => {
-
+    const loadWeeksData = async () => {
+        try {
+            const response = await fetch('/src/data/weeks.json', { mode: "cors" });
+            if (!response.ok) {
+                throw new Error(`Weekly progress data not found. Status: ${response.status}`);
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(`Error loading weekly data: ${error}`);
+            throw new Error('Failed to load weekly data. Please try again later.');
+        }
     };
 
     return { loadWeeksData };
