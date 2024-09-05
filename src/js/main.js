84a251c79af41b893c122954034f100d69f2eb10
catalinbroinas <catalinbroinas@yahoo.com>
2024-09-05 14:20:06 +0300

    Test functionality of `renderNavigation` function

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,56 @@
 import { WeekDataProvider, WeekDomManager } from "./modules/weeks.js";
 
 async function MainDomManager() {
+    // Get weekly data
+    const dataProvider = await WeekDataProvider();
+    const data = await dataProvider.loadWeeksData();
+
+    // Create the DOM manager to render the data
+    const domManager = WeekDomManager();
+
     // Load and render the data weekly
     const weeklyProgress = async () => {
         try {
-            // Get weekly data
-            const dataProvider = await WeekDataProvider();
-            const data = await dataProvider.loadWeeksData();
-
-            // Create the DOM manager and render the data
-            const domManager = WeekDomManager();
             domManager.renderWeeks(data);
         } catch (error) {
             console.error('Weekly progress data are not available! Message:', error);
         }
     };
 
+    // Load and render the data for weekly navigation
+    const weeklyNavigation = async () => {
+        try {
+            const yearData = [];
+            const monthData = [];
+
+            if (data?.length) {
+                data.forEach(yearObj => {
+                    const { year, months } = yearObj;
+                    yearData.push(year);
+
+                    if (months?.length) {
+                        months.forEach((monthObj) => {
+                            const { month } = monthObj;
+                            monthData.push(month.name);
+                        });
+                    }
+                });
+            }
+
+            domManager.renderNavigation(yearData, monthData);
+        } catch (error) {
+            console.error('Weekly navigation data are not available! Message:', error);
+        }
+    };
+
     return {
-        weeklyProgress
+        weeklyProgress,
+        weeklyNavigation
     };
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
     const domManager = await MainDomManager();
     domManager.weeklyProgress();
+    domManager.weeklyNavigation();
 });
